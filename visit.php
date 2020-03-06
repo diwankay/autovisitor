9670ee798faba4d1170c6a98d11f0f06baca1750
diwankay <36474203+diwankay@users.noreply.github.com>
2020-03-06 20:37:23 +0000

    bigo visit.php
    Start

--- visit.php
+++ visit.php
@@ -204,35 +204,7 @@ class autovisitor extends Random_UA {
 	private function acakReferer() {
 		$list = array();
 		/* Asal traffic yang di submit */ 
-		$list[] = "http://facebook.com";
-		$list[] = "http://google.com.sg";
-		$list[] = "http://twitter.com";
- 		$list[] = "http://facebook.com";
-       		 $list[] = "http://google.com.sg";
-	    $list[] = "http://twitter.com";
-	    $list[] = "http://google.co.id";
-	    $list[] = "http://google.com.my";
-	    $list[] = "http://google.jp";
-	    $list[] = "http://google.us";
-	    $list[] = "http://google.tl";
-	    $list[] = "http://google.ac";
-	    $list[] = "http://google.ad";
-	    $list[] = "http://google.ae";
-	    $list[] = "http://google.af";
-	    $list[] = "http://google.ag";
-	    $list[] = "http://google.ru";
-	    $list[] = "http://google.by";
-	    $list[] = "http://google.ca";
-	    $list[] = "http://google.cn";
-	    $list[] = "http://google.cl";
-	    $list[] = "http://google.cm";
-	    $list[] = "http://google.cv";
-	    $list[] = "http://google.gg";
-	    $list[] = "http://google.ge";
-	    $list[] = "http://google.gr";
-	    $list[] = "http://google.com.tw";
-	    $list[] = "https://search.yahoo.com";
-	    $list[] = "http://google.id";
+		$list[] = "https://slink.bigovideo.tv/61CGem";
         $list[] = "http://www.beinyu.com";
 		$acak = array_rand($list,1);
 		return $list[$acak];
